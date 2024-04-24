fbe39de1b20c7df1c7b79fea8eca1d0326a92d6a
ajeetsalesforce <ajeet.tripathi@salesforce.com>
2024-04-24 17:05:19 +0530

    Added

---
+++ UrlShortner/src/main/java/com/telusko/contest/config/CorsConfig.java
@@ -0,0 +1,18 @@
+package com.telusko.contest.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000") // Allow requests from your local React app
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // Allow specific HTTP methods
+                .allowedHeaders("*"); // Allow all headers
+    }
+}{
+}
